917b6ed249a551eadf5f792362b53e96a22b160c
Eric J Han <30414875+SppEric@users.noreply.github.com>
2025-03-13 22:41:31 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // basePath: "/innordle  ",
-  // output: "export",  // <=== enables static exports
-  // reactStrictMode: true,
+  basePath: "/WanderingInndle",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
