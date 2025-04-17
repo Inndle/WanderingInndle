281d81b2ed03011f356babce6791ba436e740bea
Samuel Ferraro <samuel_ferraro@brown.edu>
2025-04-17 18:00:14 -0400

    Fancy Spoiler Layout 7

--- src/app/Components/SettingsModal.tsx
+++ src/app/Components/SettingsModal.tsx
@@ -196,8 +196,8 @@ export default function SettingsModal({ onClose, initialSettings, onSettingsChan
 
                             {/* No Spoilers section */}
                             <div>
-                                <div className="col-span-2 text-sm underline text-gray-600 mb-2 text-center">No Spoilers</div>
                                 <div className="grid grid-cols-2 gap-x-8 gap-y-2">
+                                <div className="col-span-2 text-sm underline text-gray-600 mb-2 text-center">No Spoilers</div>
                                     {noSpoilers.map((category, idx) => {
                                         // Center the last little dude
                                         const isLastOdd = noSpoilers.length % 2 === 1 && idx === noSpoilers.length - 1;
