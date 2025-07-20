9b90a8e579981c0425e055fdd9db514177e573a9
Eric J Han <ericjasonhan@gmail.com>
2025-07-20 10:51:09 -0400

    feat: testing addition of Google Analytics

---
+++ src/app/Components/GoogleAnalytics.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Script from 'next/script';
+
+const GA_MEASUREMENT_ID = 'G-GCVSE9SM5L';
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="ga-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+}
