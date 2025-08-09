60c1f87ffaea25559e19a860f46951b7db5f57ff
CalvinWill <98561276+CalvinWill@users.noreply.github.com>
2025-08-09 17:40:47 -0400

    InitialPublic

--- src/app/Components/Game.tsx
+++ src/app/Components/Game.tsx
@@ -76,6 +76,7 @@ function Modal({ onClose, resetFunc, setDaily, settingsModalFunc, allCharacterDa
     "2025-08-04": "Garen",
     "2025-08-05": "Fithea",
     "2025-08-08": "Seraphel",
+    "2025-08-10": "Gothica",
   };
 
   // Date without time is automatically UTC.
