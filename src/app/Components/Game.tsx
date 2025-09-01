cb094d3781a33c9be2d286039e23c63ec2c6489f
CalvinWill <98561276+CalvinWill@users.noreply.github.com>
2025-09-01 19:00:13 -0400

    cleanup

--- src/app/Components/Game.tsx
+++ src/app/Components/Game.tsx
@@ -142,8 +142,8 @@ function Modal({ onClose, resetFunc, setDaily, settingsModalFunc, allCharacterDa
     "2025-08-29": "Bethal",
     "2025-08-30": "Kenjiro",
     "2025-08-31": "Talia", // Was gonna repeat Halrac
-    "2025-09-01": "Quarass", // Was gonna repeat Halrac
-    "2025-09-02": "Satar", // Was gonna repeat Halrac
+    "2025-09-01": "Quarass",
+    "2025-09-02": "Satar",
   };
 
   // Date without time is automatically UTC.
