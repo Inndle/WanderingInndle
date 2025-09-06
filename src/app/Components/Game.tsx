5885f7c6fc4fe180ef07f0ada5b38b27516fe82a
CalvinWill <98561276+CalvinWill@users.noreply.github.com>
2025-09-06 19:47:05 -0400

    hrmm

--- src/app/Components/Game.tsx
+++ src/app/Components/Game.tsx
@@ -148,6 +148,7 @@ function Modal({ onClose, resetFunc, setDaily, settingsModalFunc, allCharacterDa
     "2025-09-04": "Visma",
     "2025-09-05": "Dakelos",
     "2025-09-06": "Wesle",
+    "2025-09-07": "Yitton",
   };
 
   // Date without time is automatically UTC.
