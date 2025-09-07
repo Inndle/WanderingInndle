dcfca0e036b061a5313c56cf36cfd40c6bbb96bc
CalvinWill <98561276+CalvinWill@users.noreply.github.com>
2025-09-07 19:58:42 -0400

    cuttingItClose

--- src/app/Components/Game.tsx
+++ src/app/Components/Game.tsx
@@ -149,6 +149,7 @@ function Modal({ onClose, resetFunc, setDaily, settingsModalFunc, allCharacterDa
     "2025-09-05": "Dakelos",
     "2025-09-06": "Wesle",
     "2025-09-07": "Yitton",
+    "2025-09-08": "Norechl",
   };
 
   // Date without time is automatically UTC.
