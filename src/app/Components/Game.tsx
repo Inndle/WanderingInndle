3f07ae7eb56fd7e5aa543bdd49672a2be5a6a0fa
CalvinWill <98561276+CalvinWill@users.noreply.github.com>
2025-09-22 19:01:34 -0400

    SafronCaulder

--- src/app/Components/Game.tsx
+++ src/app/Components/Game.tsx
@@ -164,6 +164,7 @@ function Modal({ onClose, resetFunc, setDaily, settingsModalFunc, allCharacterDa
     "2025-09-20": "Rose",
     "2025-09-21": "Mars",
     "2025-09-22": "Noass",
+    "2025-09-23": "Zeladona",
   };
 
   // Date without time is automatically UTC.
