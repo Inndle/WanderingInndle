dd87d2ba52a0ede157976ad1aca0d5c7f9b7b82e
CalvinWill <98561276+CalvinWill@users.noreply.github.com>
2025-10-14 19:32:17 -0400

    Attorney General

--- src/app/Components/Game.tsx
+++ src/app/Components/Game.tsx
@@ -186,6 +186,7 @@ function Modal({ onClose, resetFunc, setDaily, settingsModalFunc, allCharacterDa
     "2025-10-12": "Thomas",
     "2025-10-13": "Eloise",
     "2025-10-14": "Colfa",
+    "2025-10-15": "Yvlon",
   };
 
   // Date without time is automatically UTC.
