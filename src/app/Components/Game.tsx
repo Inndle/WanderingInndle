7005f5c0effc520f7c982299b476176c950b5e66
CalvinWill <98561276+CalvinWill@users.noreply.github.com>
2026-06-26 19:34:20 -0400

    Update Game.tsx

--- src/app/Components/Game.tsx
+++ src/app/Components/Game.tsx
@@ -438,6 +438,7 @@ function Modal({ onClose, resetFunc, setDaily, settingsModalFunc, allCharacterDa
     "2026-06-24": "Illphres",
     "2026-06-25": "Larracel",
     "2026-06-26": "Yazdil",
+    "2026-06-27": "Moore",
 
 
 
