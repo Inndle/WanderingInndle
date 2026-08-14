3ce87b3fcf4f350d0136f2bc43c053b1fdefd674
CalvinWill <98561276+CalvinWill@users.noreply.github.com>
2026-08-14 17:56:53 -0400

    blackfire :3

--- src/app/Components/Game.tsx
+++ src/app/Components/Game.tsx
@@ -488,6 +488,8 @@ function Modal({ onClose, resetFunc, setDaily, settingsModalFunc, allCharacterDa
     "2026-08-13": "Normen",
     "2026-08-14": "Fetohep",
     "2026-08-15": "Prost",
+    "2026-08-16": "Venitra",
+    "2026-08-17": "Maviola",
 
 
 
