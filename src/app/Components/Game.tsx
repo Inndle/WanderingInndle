dc32cb25f42f13b1d7d43a545a3793d29e74d23e
CalvinWill <98561276+CalvinWill@users.noreply.github.com>
2026-09-01 19:03:05 -0400

    coldplayer

--- src/app/Components/Game.tsx
+++ src/app/Components/Game.tsx
@@ -504,7 +504,8 @@ function Modal({ onClose, resetFunc, setDaily, settingsModalFunc, allCharacterDa
     "2026-08-29": "Nerrhavia",
     "2026-08-30": "Vaulont",
     "2026-08-31": "Jewel",
-    "2026-09-01": "Kenjiro", 
+    "2026-09-01": "Kenjiro",
+    "2026-09-02": "Nereshal",
 
 
   };
